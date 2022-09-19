db1c00db0fbcc65827ac492c621e7fa00364c28b
Данил Блинов <prototape.123456789@gmail.com>
2022-09-19 18:06:12 +0500

    Добавьте файлы проекта.

---
+++ laba 4 oop/Base.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_4_oop
+{
+    abstract class Base
+    {
+        public abstract string Name { get; init; }
+        public abstract void printName();
+
+        ~Base()
+        {
+
+        }
+    }
+}
